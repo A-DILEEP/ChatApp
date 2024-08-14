1c71d60625549e68f84384dd2be125e874c399ee
A-DILEEP <dileepalla2003@gmail.com>
2024-08-14 23:53:55 +0530

    completed building some components

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,11 +1,18 @@
-import React from 'react'
+import React from "react";
 
 const Search = () => {
   return (
-    <div>
-      Searcha
+    <div className="search">
+      <div className="searchForm">
+        <input type="text" />
+      </div>
+      <div className="userChat">
+        <img src="https://wallpapers-clan.com/wp-content/uploads/2023/01/anime-aesthetic-boy-pfp-3.jpg" />
+        <div className="userChatInfo"></div>
+        <span>Jane</span>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Search
+export default Search;
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
+import Search from '../components/Search'
 const Sidebar = () => {
   return (
     <div className='sidebar'>
       <Navbar/>
+      <Search/>
     </div>
   )
 }
