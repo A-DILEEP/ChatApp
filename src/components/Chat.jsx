d6dcbb9a62ecad1b25061917347bf4b91de9833e
A-DILEEP <dileepalla2003@gmail.com>
2024-08-17 23:29:48 +0530

    making changes in chat component

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -1,11 +1,20 @@
-import React from 'react'
-
+import React from "react";
+import Cam from '../assets/cam.png'
+import add from '../assets/add.png'
+import more from '../assets/more.png'
 const Chat = () => {
   return (
-    <div className='chat'>
-      chat
+    <div className="chat">
+      <div className="chatInfo">
+        <span>Dileep</span>
+        <div className="chatIcons">
+          <img src={Cam} altsrc="Camera"/>
+          <img src={add} altsrc="add"/>
+          <img src={more} altsrc="more"/>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Chat
+export default Chat;
