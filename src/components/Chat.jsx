95c3247beee13ff94eec9b70d97d27e1c01e3221
A-DILEEP <dileepalla2003@gmail.com>
2024-08-18 23:57:35 +0530

    chat component

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -1,18 +1,22 @@
 import React from "react";
-import Cam from '../assets/cam.png'
-import add from '../assets/add.png'
-import more from '../assets/more.png'
+import Cam from "../assets/cam.png";
+import add from "../assets/add.png";
+import more from "../assets/more.png";
+import Messages from "./Messages";
+import Input from "./Input";
 const Chat = () => {
   return (
     <div className="chat">
       <div className="chatInfo">
         <span>Dileep</span>
         <div className="chatIcons">
-          <img src={Cam} altsrc="Camera"/>
-          <img src={add} altsrc="add"/>
-          <img src={more} altsrc="more"/>
+          <img src={Cam} altsrc="Camera" />
+          <img src={add} altsrc="add" />
+          <img src={more} altsrc="more" />
         </div>
       </div>
+      <Messages />
+      <Input/>
     </div>
   );
 };
