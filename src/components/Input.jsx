e523e2dfa842c1a3dfaa44cb6a6ed8f7ccb28547
A-DILEEP <dileepalla2003@gmail.com>
2024-08-20 14:44:36 +0530

    chat

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
-
+import Img from '../assets/img.png'
+import Attach from '../assets/attach.png'
 const Input = () => {
   return (
-    <div>
-      input
+    <div className="input">
+       <input type="text" placeholder='Type something ' />
+       <div className="send">
+        <img src={Attach} alt="image" />
+        <input type="file" id="file" style={{display:'none'}} />
+        <label htmlFor="file">
+          <img src={Img} alt="file" />
+        </label>
+        <button>Send</button>
+       </div>
     </div>
   )
 }
