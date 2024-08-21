1bf3f92ed4a704ea684a72e53267410a933ce7d7
A-DILEEP <dileepalla2003@gmail.com>
2024-08-21 11:47:14 +0530

    messages

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -2,10 +2,17 @@ import React from 'react'
 
 const Message = () => {
   return (
-    <div>
-      Message
+    <div className="message">
+      <div className="messageInfo">
+        <img src="https://wallpapercave.com/wp/wp9927157.jpg" alt="boyImage" />
+        <span>Just Now</span>
+      </div>
+      <div className="messageContent">
+        <p>Hello buddies !! </p>
+        {/* <img src="https://wallpapercave.com/wp/wp9927157.jpg" alt="returnimg" /> */}
+      </div>
     </div>
-  )
+  );
 }
 
 export default Message
