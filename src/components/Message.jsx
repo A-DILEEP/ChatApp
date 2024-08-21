0e70b45145da95d791df33a13c6a21f80995da7e
A-DILEEP <dileepalla2003@gmail.com>
2024-08-21 23:15:19 +0530

    chat

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -2,14 +2,14 @@ import React from 'react'
 
 const Message = () => {
   return (
-    <div className="message">
+    <div className="message owner">
       <div className="messageInfo">
         <img src="https://wallpapercave.com/wp/wp9927157.jpg" alt="boyImage" />
         <span>Just Now</span>
       </div>
       <div className="messageContent">
         <p>Hello buddies !! </p>
-        {/* <img src="https://wallpapercave.com/wp/wp9927157.jpg" alt="returnimg" /> */}
+        <img src="https://wallpapercave.com/wp/wp9927157.jpg" alt="returnimg" />
       </div>
     </div>
   );
