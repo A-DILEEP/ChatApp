7eb57bc77fe1f3f1b68022e808fc897f367c5d88
A-DILEEP <dileepalla2003@gmail.com>
2024-08-27 21:48:24 +0530

    ..

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+
+import { initializeApp } from "firebase/app";
+const firebaseConfig = {
+  apiKey: "AIzaSyDbls9OtcLOup7G4EQUzGDb-UUWZYCIFPs",
+  authDomain: "chat-app-63a31.firebaseapp.com",
+  projectId: "chat-app-63a31",
+  storageBucket: "chat-app-63a31.appspot.com",
+  messagingSenderId: "400700338197",
+  appId: "1:400700338197:web:a063bb6d1304646d895e1c",
+};
+
+const app = initializeApp(firebaseConfig);
