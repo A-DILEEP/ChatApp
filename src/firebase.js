d86e285636ab9ac961c24650ceb05205b6a88bdc
A-DILEEP <dileepalla2003@gmail.com>
2024-10-19 09:32:01 +0530

    .

--- src/firebase.js
+++ src/firebase.js
@@ -3,12 +3,7 @@ import { getAuth } from "firebase/auth";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAC3fuazqHamfAnozia5mfA6NczSSTjBLo",
-  authDomain: "chatapp-fe868.firebaseapp.com",
-  projectId: "chatapp-fe868",
-  storageBucket: "chatapp-fe868.appspot.com",
-  messagingSenderId: "465158491566",
-  appId: "1:465158491566:web:c525f3105f2fe623b90c3f",
+
 };
 
 export const app = initializeApp(firebaseConfig);
