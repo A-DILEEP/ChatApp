64757cc09f10c8a6b55e5b090f7dd6e360152a01
A-DILEEP <dileepalla2003@gmail.com>
2024-10-19 09:41:05 +0530

    ignoring firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -2,8 +2,9 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getStorage } from "firebase/storage";
 
+const apikey=process.env.apikey;
 const firebaseConfig = {
-
+  
 };
 
 export const app = initializeApp(firebaseConfig);
